b5de9018cb71cfa85ec1e1190bc6d07ade438c38
gillygabyte <gillygabyte@gmail.com>
2023-11-16 23:06:03 -0800

    Update table.tsx

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -85,7 +85,7 @@ export default async function InvoicesTable({
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
                       <Image
-                      alt="picture"
+                        alt="picture"
                         src={invoice.image_url}
                         className="rounded-full"
                         width={28}
